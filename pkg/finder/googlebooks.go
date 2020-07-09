5fa0526ce5eaf85d40e521d80fa29794d787bd33
Faithfulness Alamu <vaguemail369@gmail.com>
2020-07-09 20:37:07 +0100

    Create the google books finder and

---
+++ pkg/finder/googlebooks.go
@@ -0,0 +1,11 @@
+package finder
+
+import "github.com/thealamu/bookfinder/internal/pkg/bookdetails"
+
+type GoogleBooks struct{}
+
+func NewGoogleBooksFinder() *GoogleBooks {
+	return &GoogleBooks{}
+}
+
+func (g *GoogleBooks) Find(s string) *bookdetails.BookDetails {}
