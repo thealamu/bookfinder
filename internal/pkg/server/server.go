9b21d875e6b35760fb0de7e2c3b4b1561044756f
Faithfulness Alamu <vaguemail369@gmail.com>
2020-07-09 18:48:44 +0100

    Use function to join host and port in server

--- internal/pkg/server/server.go
+++ internal/pkg/server/server.go
@@ -3,12 +3,13 @@ package server
 import (
 	"context"
 	"fmt"
+	"net"
 	"net/http"
 )
 
 // StartServer starts the server using the environment
 func StartServer(ctx context.Context, env *ServerEnv) {
-	addr := ":" + env.Port
+	addr := net.JoinHostPort("localhost", env.Port)
 	fmt.Println("Starting server on", addr)
 	err := http.ListenAndServe(addr, env.Handler)
 	if err != nil {
